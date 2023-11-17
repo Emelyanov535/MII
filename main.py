aed05837be1410e2482130f1449c0591aa27ff87
ArtemEmelyanov <artem_emelyanov_2003@list.ru>
2023-11-17 15:31:19 +0400

    Емае, забыл перенести 4 лабу а новую ветку

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 
+from BloomFilter import BloomFilter
 from generateData import expand_dataset
 from tableFun import dataForTable, groupData, diagramm
 
@@ -8,6 +9,22 @@ app = Flask(__name__)
 data = pd.read_csv('data.csv')
 expand_dataset(data)
 expandedData = pd.read_csv('expanded_data.csv')
+bloom_filter = BloomFilter(1000000, 100000)
+
+v7 = ('stock index; country; year; index price; log_indexprice; inflationrate; oil prices; exchange_rate; '
+      'gdppercent; percapitaincome; unemploymentrate; manufacturingoutput; tradebalance; USTreasury; NASDAQ; '
+      'FTSE 100; Nifty 50; Nikkei 225; HSI; SZCOMP; DAX 30; CAC 40; IEX 35; United States of America; United Kingdom; '
+      'India; Japan; Hong Kong; China; Germany; France; Spain').lower()
+
+v17 = ('Price; Levy; Manufacturer; Model; year; Category; Leather interior; Fuel type; Engine volume; Mileage; '
+       'Cylinders; Gear box type; Drive wheels; Doors; Wheel; Color; Airbags').lower()
+
+kaggles = {'https://www.kaggle.com/datasets/pratik453609/economic-data-9-countries-19802020': v7.split('; '),
+           'https://www.kaggle.com/datasets/deepcontractor/car-price-prediction-challenge': v17.split('; ')}
+
+for v in kaggles.values():
+    for i in v:
+        bloom_filter.add_to_filter(i.lower())
 
 
 @app.route('/')
@@ -60,10 +77,29 @@ def analyzeExpanded():
                            unemploymentrate_by_country=fun[2],
                            unemploymentrate_by_year=fun[3])
 
-#
-diagramm(data)
-diagramm(expandedData)
 
+@app.route('/filter', methods=['GET'])
+def bloom_filter_page():
+    return render_template('filter.html')
+
+
+@app.route('/filter', methods=['POST'])
+def apply_bloom_filter():
+    if request.method == 'POST':
+        user_input = request.form.get('user_input', '').lower()
+        kaggle_link = None
+        if bloom_filter.check_is_not_in_filter(user_input):
+            result_text = f'The term "{user_input}" does not exist in the dataset.'
+        else:
+            for kaggle_url, dataset_terms in kaggles.items():
+                if user_input in dataset_terms:
+                    kaggle_link = kaggle_url
+                    result_text = f'"{user_input}" may exist in the dataset.'
+                    break
+            else:
+                result_text = f'"{user_input}" does not exist in the dataset.'
+
+        return jsonify({'result_text': result_text, 'kaggle_link': kaggle_link})
 
 
 if __name__ == "__main__":
